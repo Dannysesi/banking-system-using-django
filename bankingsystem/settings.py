53c2290c062fa8543c1cd84e6647ad6ac854f7d9
Dannysesi <daniel.olusesi20@gmail.com>
2024-10-31 06:27:38 +0100

    settings modification

--- bankingsystem/settings.py
+++ bankingsystem/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user.apps.UserConfig',
+    'bank.apps.BankConfig',
     'crispy_forms',
     'crispy_bootstrap5'
 ]
@@ -132,6 +133,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_URL = '/login/'
+
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
